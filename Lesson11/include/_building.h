88fd413e2b6010b1dec489bf271d8c0c3874d6a7
pmagnuson17 <phillipmagnuson17@mail.fresnostate.edu>
2020-05-13 17:03:23 -0700

    building(in progress)

---
+++ Lesson11/include/_building.h
@@ -0,0 +1,16 @@
+#ifndef _BUILDING_H
+#define _BUILDING_H
+
+
+class _building
+{
+    public:
+        _building();
+        virtual ~_building();
+
+    protected:
+
+    private:
+};
+
+#endif // _BUILDING_H
---
+++ Lesson11/src/_building.cpp
@@ -0,0 +1,11 @@
+#include "_building.h"
+
+_building::_building()
+{
+    //ctor
+}
+
+_building::~_building()
+{
+    //dtor
+}
